ce994411b7cb82f27239e008fcf2f9e6c6e1016c
jkkim113 <rlawhdrud113@gmail.com>
2023-06-16 11:40:06 +0900

    First Commit

---
+++ MonhunLike/Source/MonhunLike/MonhunLikeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "MonhunLikeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MONHUNLIKE_API AMonhunLikeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
